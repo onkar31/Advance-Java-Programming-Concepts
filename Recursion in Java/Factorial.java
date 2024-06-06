4f980dea55c7da68f7d170eb52e95caf8706ba81
onkar31 <onkarbhogle08@gmail.com>
2024-06-06 07:36:39 +0530

    method name updated

--- Recursion in Java/Factorial.java
+++ Recursion in Java/Factorial.java
@@ -2,15 +2,15 @@
 public class Factorial {
     public static void main(String[] args) {
         int num = 5;
-        long result = factorial(num);
+        long result = getFactorial(num);
         System.out.println("factorial of a "+ num + " is: "+ result);
     }
 
-    public static long factorial(int n){
+    public static long getFactorial(int n){
         if(n == 0){
             return 1;   // Base case
         }else{
-            return n * factorial(n - 1);    // Recursive case
+            return n * getFactorial(n - 1);    // Recursive case
         }
     }
 }
