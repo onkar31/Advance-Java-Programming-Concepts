cb52ac5a2f7c5910dfd1c699226661760f8e2275
onkar31 <onkarbhogle08@gmail.com>
2024-06-06 08:47:13 +0530

    Sorted Array using Recursion

---
+++ Recursion in Java/SortedArray.java
@@ -0,0 +1,16 @@
+public class SortedArray {
+    public static void main(String[] args) {
+        int arr[] = {1, 2, 3, 4, 6, 5};
+        System.out.println(isSorted(arr, 0));   // index = 0
+    }
+
+    public static boolean isSorted(int arr[], int i){
+        if(i == arr.length-1){
+            return true;    // Base case
+        }
+        if(arr[i] > arr[i+1]){
+            return false;
+        }
+        return isSorted(arr, i+1);  // Recursive case
+    }
+}
